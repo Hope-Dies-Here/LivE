5fb7a046069bf1a51bca8b1a344f48c16c715eaa
Dummy <onyourleftpal@gmail.com>
2023-05-06 10:58:57 +0300

    run time

--- Router/router.js
+++ Router/router.js
@@ -16,14 +16,15 @@ const validUser = (req, res, next) => {
 
 router.get("/", async (req, res) => {
    // Hash a password
-  /*let hPassword = "user";
+  /* let hPassword = "bbc";
   bcrypt.genSalt(10, (err, salt) => {
     bcrypt.hash(hPassword, salt, async (err, hash) => {
       console.log(hash);
-      await users.updateOne({username: "user"}, {$set : {password: hash}})
+      await users.updateOne({username: "Zlelew"}, {$set : {password: hash}})
       // Store the hash in the database
     });
-  }); */
+  }); 
+    */
   res.render("index", {
     page: "home",
     show: req.session.showProNav,
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const compression = require("compression")
 const helmet = require("helmet")
 const mongoose = require("mongoose")
 const session = require("express-session")
+const MemoryStore = require("memorystore")(session);
 const ejs = require("ejs")
 const bodyParser = require("body-parser")
 const app = express()
@@ -10,7 +11,7 @@ const router = require("./Router/router.js")
 const port = process.env.PORT || 3000
 require("dotenv").config()
 
-const dbString = process.env.DB_STRING || DB_STRING
+const dbString = process.env.DB_STRING
 mongoose.connect(dbString)
 const db = mongoose.connection
 // db.on("error", console.log("error connecting db"))
@@ -21,7 +22,11 @@ db.once("open", () => {
 
 app.use(session({
   secret: "getto bird",
-  resave: true,
+  cookie: { maxAge: 86400000 },
+  store: new MemoryStore({
+    checkPeriod: 86400000
+  }),
+  resave: false,
   saveUninitialized: true
 }))
 
