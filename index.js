adfa8e38114df993a13bd685d28721794f798d3a
Dummy <onyourleftpal@gmail.com>
2023-06-18 09:29:49 +0300

    first deployment

--- index.js
+++ index.js
@@ -71,6 +71,7 @@ app.use(compression());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public/"));
 //app.use(express.static(path.join(__dirname, 'public')))
+
 // app.use("/", router)
 app.use("/demo", demo);
 
